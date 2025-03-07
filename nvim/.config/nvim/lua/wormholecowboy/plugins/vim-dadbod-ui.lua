8f69d927ff86f84dbee1cc084ab4e35f51c0d7aa
wormholecowboy <wormholecowboy@gmail.com>
2025-03-07 14:18:47 -0500

    vim dadbod

--- nvim/.config/nvim/lua/wormholecowboy/plugins/vim-dadbod-ui.lua
+++ nvim/.config/nvim/lua/wormholecowboy/plugins/vim-dadbod-ui.lua
@@ -1,17 +1,26 @@
 return {
-  'kristijanhusak/vim-dadbod-ui',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	init = function()
+		-- Your DBUI configuration
+		vim.g.db_ui_use_nerd_fonts = 1
+
+		vim.g.dbs = {
+			stagePublic = os.getenv("DBUI_STAGEPUBLIC"),
+			prodPublic = os.getenv("DBUI_PRODPUBLIC"),
+			stageGeneral = os.getenv("DBUI_STAGEGENERAL"),
+			prodGeneral = os.getenv("DBUI_PRODGENERAL"),
+		}
+
+    vim.keymap.set("n", "<leader>ddb", ":DBUIToggle<cr>", {})
+	end,
 }
