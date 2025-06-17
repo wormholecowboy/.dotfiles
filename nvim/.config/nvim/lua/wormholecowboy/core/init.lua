68198ff72fa4f85a1186e0fa74c60fdfb3d3ecd4
wormholecowboy <wormholecowboy@gmail.com>
2025-06-17 12:04:21 -0400

    Added win32yank to nvim config

--- nvim/.config/nvim/lua/wormholecowboy/core/init.lua
+++ nvim/.config/nvim/lua/wormholecowboy/core/init.lua
@@ -2,6 +2,22 @@ require('wormholecowboy.core.keymaps')
 require('wormholecowboy.core.options')
 require('wormholecowboy.core.autocommands')
 
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
+
+
 vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
 vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
 vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
