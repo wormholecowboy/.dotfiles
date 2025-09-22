26b6aec824a3593d3b1288f943c67b7a37104db0
WormholeCowboy <wormholecowboy@gmail.com>
2025-09-22 16:41:34 -0400

    Updated oil bindings

--- nvim/.config/nvim/lua/wormholecowboy/plugins/oil.lua
+++ nvim/.config/nvim/lua/wormholecowboy/plugins/oil.lua
@@ -68,7 +68,7 @@ return {
       ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
       ["<C-t>"] = { "actions.select", opts = { tab = true } },
       ["<C-p>"] = "actions.preview",
-      ["<C-c>"] = { "actions.close", mode = "n" },
+      ["q"] = { "actions.close", mode = "n" },
       ["<C-l>"] = "actions.refresh",
       ["-"] = { "actions.parent", mode = "n" },
       ["_"] = { "actions.open_cwd", mode = "n" },
@@ -213,12 +213,7 @@ return {
 
     local openState = false
     vim.keymap.set("n", "<leader>e", function()
-      -- openState = not openState
-      -- if openState then
-      oil.open()
-      -- else
-      -- oil.close()
-      -- end
+      oil.toggle_float()
     end, { desc = "Toggle Oil" })
   end,
 }
