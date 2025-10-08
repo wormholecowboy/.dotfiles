8e954175c716a6fb2f092afaae45984afe52f59a
WormholeCowboy <wormholecowboy@gmail.com>
2025-10-08 17:35:36 -0400

    added harpoon

---
+++ nvim/.config/nvim/lua/wormholecowboy/plugins/harpoon.lua
@@ -0,0 +1,33 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", ",g", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", ",a", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", ",s", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", ",d", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", ",f", function()
+			harpoon:list():select(4)
+		end)
+		vim.keymap.set("n", ",q", function()
+			harpoon:list():select(5)
+		end)
+
+	end,
+}
