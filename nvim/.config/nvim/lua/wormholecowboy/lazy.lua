f88f6aaa75098c9c2c612174e0af0a2aa8ab0be4
WormholeCowboy <wormholecowboy@gmail.com>
2025-11-18 11:15:16 -0500

    Lazy config checker added

--- nvim/.config/nvim/lua/wormholecowboy/lazy.lua
+++ nvim/.config/nvim/lua/wormholecowboy/lazy.lua
@@ -11,7 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "wormholecowboy.plugins" }, {import = "wormholecowboy.plugins.lsp"} }, {
+require("lazy").setup({
+	spec = {
+		{ import = "wormholecowboy.plugins" },
+		{ import = "wormholecowboy.plugins.lsp" },
+	},
+  checker = {
+    enabled = true,
+    notify = false,
+  },
 	change_detection = {
 		notify = false,
 	},
