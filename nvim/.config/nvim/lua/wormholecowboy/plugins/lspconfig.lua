d156ab234f3fec71fa80382e3ac2307c6e839494
WormholeCowboy <wormholecowboy@gmail.com>
2025-11-19 11:54:53 -0500

    Addded lsp icons

--- nvim/.config/nvim/lua/wormholecowboy/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/wormholecowboy/plugins/lspconfig.lua
@@ -28,7 +28,7 @@ return {
 		})
 
 		-- Set up diagnostic signs
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		local signs = { Error = "⛔", Warn = "⚠️", Hint = "🔬", Info = "ℹ️" }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
