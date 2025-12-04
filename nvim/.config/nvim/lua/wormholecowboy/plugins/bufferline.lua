0eaef989e660ead7efce185665f314179c0c9049
WormholeCowboy <wormholecowboy@gmail.com>
2025-12-04 14:37:29 -0500

    nvim: fix bufferline event

--- nvim/.config/nvim/lua/wormholecowboy/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/wormholecowboy/plugins/bufferline.lua
@@ -2,6 +2,7 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
+  event = { "BufReadPre", "BufNewFile" },
   keys = {
     { "[b", "<cmd>BufferLineMovePrev<cr>", desc = "move buffer left" },
     { "]b", "<cmd>BufferLineMoveNext<cr>", desc = "move buffer right" },
