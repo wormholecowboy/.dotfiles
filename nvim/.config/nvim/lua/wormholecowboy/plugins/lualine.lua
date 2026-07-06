c401be4912802721eb3eabe5cb0f0b27e9fa66fb
Wormhole Cowboy <wormholecowboy@gmail.com>
2026-07-06 11:03:31 -0400

    feat(nvim): statusbar rounded

--- nvim/.config/nvim/lua/wormholecowboy/plugins/lualine.lua
+++ nvim/.config/nvim/lua/wormholecowboy/plugins/lualine.lua
@@ -11,7 +11,7 @@ return {
         icons_enabled = true,
         theme = "codedark",
         component_separators = { left = "|", right = "|" },
-        section_separators = { left = "", right = "" },
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {
           statusline = {},
           winbar = {},
@@ -26,21 +26,16 @@ return {
         },
       },
       sections = {
-        lualine_a = { "mode" },
+        lualine_a = {
+          { 'mode', separator = { left = '', right = '' } }
+        },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { {get_current_buffer_path}},
-        -- lualine_c = {
-        --   {
-        --     "buffers",
-        --     buffers_color = {
-        --       -- active = { fg = '#ffaa88', bg = 'grey', gui='italic,bold' }
-        --       active = "Visual",
-        --     },
-        --   },
-        -- },
+        lualine_c = { { get_current_buffer_path } },
         lualine_x = { "filetype" },
         lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_z = {
+          { 'location', separator = { left = '', right = '' } },
+        },
       },
       inactive_sections = {
         lualine_a = {},
